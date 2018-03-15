dea52a7ed25a7e1f2d4af3cd205edf83319f5b72
ioanabirsan <ioana.c.birsan@gmail.com>
2018-03-15 22:04:33 +0200

    Add basic structure for menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 function showPage(selectedPage){
-	var pages = ['home', 'books', 'articles', 'videos', 'presentations'];
+	var pages = document.getElementsByClassName("page");
 
 	var index;
 	for(index = 0; index < pages.length; index++){
-		var pageID = pages[index];
-		var element = document.getElementById(pageID);
-		element.style.display = 'none';
+		var page = pages[index];
+		page.style.display = 'none';
 	}
 
-	var elementToShow = document.getElementById(selectedPage);
-	elementToShow.style.display = 'block';
+	var pageToShow = document.getElementById(selectedPage);
+	pageToShow.style.display = 'block';
 }
