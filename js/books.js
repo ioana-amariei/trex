3a5d0b46537622cec2e012a9d535a897fee2bd66
ioanabirsan <ioana.c.birsan@gmail.com>
2018-03-17 22:36:24 +0200

    Add display type selection functionality

--- js/books.js
+++ js/books.js
@@ -1,4 +1,4 @@
-function selectCurrentDisplayType(displayType) {
+function selectCurrentDisplayType(displayType, classToAdd, classToRemove) {
   var displayTypes = document.getElementsByClassName("selected-display-type");
 
   var index;
@@ -7,4 +7,11 @@ function selectCurrentDisplayType(displayType) {
   }
 
   displayType.classList.add("selected-display-type");
+
+  var resources = document.getElementsByClassName("resource");
+
+  for(index=0; index < resources.length; index++){
+    resources[index].classList.remove(classToRemove);
+    resources[index].classList.add(classToAdd);
+  }
 }
