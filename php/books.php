fbebbf7f58381fd59e46e9ac7e1695caae8269da
ioanabirsan <ioana.c.birsan@gmail.com>
2018-03-19 20:53:48 +0200

    Add responsive design and create skeleton for http books request

--- php/books.php
+++ php/books.php
@@ -8,6 +8,17 @@ $book->setTitle("The Java Programming Guide");
 $books = [$book, $book];
 $response = ["resources" => $books];
 
-echo json_encode($response);
+// echo json_encode($response);
+//$resp = http_get("https://www.googleapis.com/books/v1/volumes?q=java", array("timeout"=>1), $info);
+//echo $resp;
 
+
+$ch = curl_init("https://www.googleapis.com/books/v1/volumes?q=java");
+
+
+$res = curl_exec($ch);
+curl_close($ch);
+
+
+echo $res;
  ?>
