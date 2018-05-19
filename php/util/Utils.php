fd8cde11a163d6576fe4b91f649dcc2a806239f8
alexluca97 <alexluca97@yahoo.com>
2018-05-19 14:49:42 +0300

    Add function fetchdata with token

--- php/util/Utils.php
+++ php/util/Utils.php
@@ -2,27 +2,37 @@
 
 class Utils {
     public static function fetchData($uri){
-        // Reference: http://thisinterestsme.com/send-get-request-with-php/
-        //Initialize cURL.
         $ch = curl_init();
 
-        //Set the URL that you want to GET by using the CURLOPT_URL option.
         curl_setopt($ch, CURLOPT_URL, $uri);
-
-        //Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        //Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
         try {
-            //Execute the request.
             $data = curl_exec($ch);
         } catch (Exception $e) {
             return [];
         }
 
-        //Close the cURL handle.
+        curl_close($ch);
+
+        return $data;
+    }
+
+    public static function fetchDataWithToken($uri, $tokenAuthorization){
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $tokenAuthorization ));
+        curl_setopt($ch, CURLOPT_URL, $uri);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        try {
+            $data = curl_exec($ch);
+        } catch (Exception $e) {
+            return [];
+        }
+
         curl_close($ch);
 
         return $data;
