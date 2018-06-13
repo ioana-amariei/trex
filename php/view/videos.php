95e0d0576f3d68fb69856cd47047d4e03113d577
alexluca97 <alexluca97@yahoo.com>
2018-06-13 21:49:40 +0300

    add initialImage Videos

--- php/view/videos.php
+++ php/view/videos.php
@@ -2,13 +2,13 @@
     require_once ('../controller/Videos.php');
     
     $v = new Videos();
-    $results = "";
 
-    if(isset($_REQUEST['termen']))
+    if(isset($_REQUEST['termen'])){
+
         $results = $v -> searchMore($_REQUEST['termen'], $_REQUEST['per_page']);
-
+    }
+        header('Content-Type: application/json');
+        http_response_code(200);
+        echo json_encode($results);
     
-    header('Content-Type: application/json');
-    http_response_code(200);
-    echo json_encode($results);
 ?>
