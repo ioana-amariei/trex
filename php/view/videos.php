f67deb137333619e4205d75e2fbb70ca05e83acd
Octav <octav.gensthaler@gmail.com>
2018-06-14 22:16:55 +0300

    Make videos return 400 if parameters aren't there

--- php/view/videos.php
+++ php/view/videos.php
@@ -3,9 +3,13 @@
 
     $v = new Videos();
 
-    if(isset($_REQUEST['termen'])){
-
+    if(isset($_REQUEST['termen']) && $_REQUEST['per_page']){
         $videos = $v -> searchMore($_REQUEST['termen'], $_REQUEST['per_page']);
+    }
+    else {
+        header('Content-Type: application/json');
+        http_response_code(400);
+        die();
     }
 
     $result = ["videos" => $videos];
